8196efa1ff8281d89e98eed89b1d714ee689bca6
Anum Kousar <160739510+anum-349@users.noreply.github.com>
2024-09-22 13:04:01 +0500

    Add files via upload

---
+++ Resume_Builder.py
@@ -0,0 +1,30 @@
+
+import streamlit as st
+
+st.title("ANUM KOUSAR")
+st.header("Digital Resume")
+
+st.subheader("Personal Information")
+st.write("Email: anumkousar552@gmail.com")
+st.write("**Phone:** +92 3495445560")
+st.write("**LinkedIn:** [https://www.linkedin.com/in/anum-kousar-984406294/](https://www.linkedin.com/in/anum-kousar-984406294/)")
+
+st.subheader("Experience")
+st.write("**Intern at DEN**")
+st.write("*Dates:* July 2024 - July 2024")
+st.write("Ihave completed my 4 c++ projects at DEN. I gained real world application experience also.")
+
+
+st.subheader("Education")
+st.write("**University** - International Islamic University Islamabad")
+st.write("*Graduation Year:* 2026")
+st.write("*Degree*- Software Engineering")
+
+st.subheader("Skills")
+st.write("- Problem Solving")
+st.write("- Project Management")
+st.write("- Team Management")
+
+st.subheader("Projects")
+st.write("**Library Mangement System**")
+st.write("Our system is designed to manage the book records and maintain the information of library patrons. It tracks the number of books in the library, how many books have been issued, whether they have been returned or not, if they have been renewed, and the charges for late book returns.It was my university project, and it was successful. ")
